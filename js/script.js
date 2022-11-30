73fde90f864f90b9ae37c0e12dc78cc139a9d594
Claudio-Emmolo <claudioemmolo90@gmail.com>
2022-11-30 16:04:19 +0100

    Add EventListener on click box

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@
 // 1 - Prendo l'elemento pulsante e il div che conterrà i "square" dall'html;
 
 // 2 - Al click del bottone, con addEventList, genero con un ciclo for per 100 "square" e aggiungo la classe "div.square"; ==> Chiamata da una funzione?
-    // 2.1 Aggiungo un altro addEventList per cambiare il bg-color della cella al click;
+// 2.1 Aggiungo un altro addEventList per cambiare il bg-color della cella al click;
 
 // 3 - Dopo aver generato i "square", con la funzione ".innerText" aggiungo i numeri generati da [i] (+ 1) OPPURE faccio partire il for da (i = 1 ; i <= 100);
 
@@ -21,6 +21,9 @@ btn.addEventListener('click', function(){
 
     for (let i = 0 ; i < 100 ; i++){
         const AddDiv = getDivElement();
+        AddDiv.addEventListener('click', function(){
+            console.log ("Hai cliccato");
+        })
         AddDiv.innerText = (i + 1);
         squareBox.append(AddDiv);
     
@@ -34,5 +37,6 @@ btn.addEventListener('click', function(){
 
 function getDivElement (){
     const divElement = document.createElement ("div");
+    divElement.classList.add("square");
     return divElement;
 }
